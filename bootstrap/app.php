f99837f521740bc89811469f06146599a8af8bb3
Ryan de Jonge <ryandejonge@outlook.com>
2024-07-21 04:25:40 +0200

    Trust Proxies!

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Middleware\TrustProxies;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -15,7 +16,9 @@ return Application::configure(basePath: dirname(__DIR__))
             \App\Http\Middleware\HandleInertiaRequests::class,
             \Illuminate\Http\Middleware\AddLinkHeadersForPreloadedAssets::class,
         ]);
-
+        $middleware->use([
+            'trustProxies' => TrustProxies::class,
+        ]);
         //
     })
     ->withExceptions(function (Exceptions $exceptions) {
